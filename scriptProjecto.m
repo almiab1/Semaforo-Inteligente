e78387fdbeedc2331af2c74d391200ce0347197e
almiab1 <almiab1@epsg.upv.es>
2019-12-11 09:20:13 +0100

    Anyadido script inicial y platilla base con imagen prueba

---
+++ scriptProjecto.m
@@ -0,0 +1,10 @@
+%% -------------------------------------------------------------------------------------------------
+%
+% Projecto Practicas TDI
+% Asignatura: Tratamiento de Imagen: Visión Artificial
+% Autores: Alejandro Mira Abad y Brian Calabuig Chafer
+% Descripción: Detección de vehiculos y personas para cambiar el estado de un semáforo
+%
+%% -------------------------------------------------------------------------------------------------
+%
+
