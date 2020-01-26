20e2fdb64a460c7acc59cb3b3025e7ef6940d220
almiab1 <almiab1@epsg.upv.es>
2020-01-26 21:51:55 +0100

    Revert "cambio gui"
    This reverts commit c4dff570255ffe44eafb20daae329a856a5b9585.

--- scripts/scriptProjecto.m
+++ scripts/scriptProjecto.m
@@ -13,19 +13,23 @@ close all
 
 
 I = imread('imagPasoCebra2.jpg'); % Cargamos la imagen
-I = rgb2gray(I);
 detectorVehicle = vehicleDetectorACF('front-rear-view'); % Cargamos el detector de vehiculos
 detectorPeople = peopleDetectorACF('inria-100x41'); % Cargamos el detector de personas
 %% -------------------------------------------------------------------------------------------------
 % Deteccion vehiculos
 [cajasVehiculos,puntuacionVehiculos] = detect(detectorVehicle,I); % Activamos el detector
 
+% pV = puntuacionVehiculos;
+% cV = cajasVehiculos;
+
+tamanyoPunt = abs(length(puntuacionVehiculos));
+
 indice = find(puntuacionVehiculos >= 5);
 
 pV = puntuacionVehiculos(indice);
 cV = cajasVehiculos(indice,:);
 
-I = insertObjectAnnotation(I,'rectangle',cV,pV); % Insertamos loos contenedores con los valores 
+% I = insertObjectAnnotation(I,'rectangle',cV,pV); % Insertamos loos contenedores con los valores 
 
 % figure
 % imshow(I)
@@ -35,20 +39,19 @@ I = insertObjectAnnotation(I,'rectangle',cV,pV); % Insertamos loos contenedores 
 [cajasPersonas,puntuacionPersonas] = detect(detectorPeople,I);
 
 if not (isempty(cajasPersonas))
-    I = insertObjectAnnotation(I,'rectangle',cajasPersonas,puntuacionPersonas);
+%     I = insertObjectAnnotation(I,'rectangle',cajasPersonas,puntuacionPersonas);
 end
 
-figure(2)
-imshow(I)
-title('Detección de vehiculos y personas con su puntuación')
+% figure
+% imshow(I)
+% title('Detección de vehiculos y personas con su puntuación')
 %% -------------------------------------------------------------------------------------------------
 % Poner en gray scale y operaciones binarias
 
 I = rgb2gray(I);
 I = imbinarize(I, 'adaptive','Sensitivity',0.4);
-p = regionprops(I,'Extrema');
-% I = imfill(I,'holes');
+I = imfill(I,'holes');
+SE = strel('rectangle',[5,4]);
+I = imdilate(I,SE);
 figure
 imshow(I)
-hold on
-plot(p(1,:))
