911b1b52bc8ed2a4b6f654d0b4a5dd868dd96edc
almiab1 <almiab1@epsg.upv.es>
2020-01-27 20:51:40 +0100

    cambio menor

--- scripts/calcularDistancia.m
+++ scripts/calcularDistancia.m
@@ -40,8 +40,8 @@ function output = calcularDistancia(imagenBase)
     y = R(P(:,1));
     lines = houghlines(BW, T, R, P, 'FillGap', 5, 'MinLength', 7);
 
-    figure
-    imshow(BW), hold on
+%     figure
+%     imshow(BW), hold on
 
     max_len = 0;
     distanciaCoche = 10000000;
@@ -49,9 +49,9 @@ function output = calcularDistancia(imagenBase)
     
     for k = 1: length(lines)
         xy = [lines(k).point1; lines(k).point2];
-        plot(xy(:,1), xy(:,2), 'LineWidth', 2, 'Color', 'green');
-        plot(xy(1,1), xy(1,2), 'x', 'LineWidth', 2, 'Color', 'yellow');
-        plot(xy(2,1), xy(2,2), 'x', 'LineWidth', 2, 'Color', 'red');
+%         plot(xy(:,1), xy(:,2), 'LineWidth', 2, 'Color', 'green');
+%         plot(xy(1,1), xy(1,2), 'x', 'LineWidth', 2, 'Color', 'yellow');
+%         plot(xy(2,1), xy(2,2), 'x', 'LineWidth', 2, 'Color', 'red');
         len = norm(lines(k).point1 - lines(k).point2);
         if (len > max_len)
          max_len = len;
